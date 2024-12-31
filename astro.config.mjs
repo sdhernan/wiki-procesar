6df6e889c8ccd013d5055e835f3eaa22b5496a60
Sadot Nahum Hernandez Moreno <sdhernan@procesar.com>
2024-12-31 15:45:35 -0600

    ✨ feat: agregar documentación de API y guías, con actualizaciones de configuración y dependencias
    Se agregó un nuevo archivo de documentación para la API de verificación de correo en src/content/docs/api/verificar-correo.mdoc, y se actualizaró la guía de introducción en src/content/docs/guias/introduccion.mdoc. Además, se realizaron modificaciones en el archivo de configuración astro.config.mjs y en el archivo package.json, lo que resultó en cambios en el archivo package-lock.json. Estos cambios incluyen tanto modificaciones locales como diferencias con la rama main, y abarcan mejoras en la documentación, configuración y dependencias del proyecto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,58 +8,53 @@ import markdoc from "@astrojs/markdoc";
 export default defineConfig({
   site: "https://sdhernan.github.io",
   base: "wiki-procesar/",
-  integrations: [
-    starlight({
-      title: "Wiki Docs",
-      defaultLocale: "root",
-      editLink: {
-        baseUrl: 'https://github.com/sdhernan/wiki-procesar/edit/main/',
+  integrations: [starlight({
+    title: "Wiki Docs",
+    defaultLocale: "root",
+    locales: {
+      root: {
+        label: "Español",
+        lang: "es",
       },
-      locales: {
-        root: {
-          label: "Español",
-          lang: "es",
-        },
+    },
+    social: {
+      github: "https://github.com/sdhernan",
+    },
+    sidebar: [
+      {
+        label: "Comienza a explorar",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: "Introducción", slug: "guias/introduccion" },
+          {
+            label: "Documentación ecológica",
+            slug: "guias/impacto-ecologico",
+          },
+        ],
       },
-      social: {
-        github: "https://github.com/sdhernan",
+      {
+        label: "Documentos",
+        items: [
+          // Each item here is one entry in the navigation menu.
+        ],
       },
-      sidebar: [
-        {
-          label: "Comienza a explorar",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Introducción", slug: "guias/introduccion" },
-            {
-              label: "Documentación ecológica",
-              slug: "guias/impacto-ecologico",
-            },
-          ],
-        },
-        {
-          label: "Documentos",
-          items: [
-            // Each item here is one entry in the navigation menu.
-          ],
-        },
-        {
-          label: "Servicios REST",
-          items: [
-            // Each item here is one entry in the navigation menu.
-          ],
-        },
-        {
-          label: "Referencias",
-          autogenerate: { directory: "reference" },
-        },
-        {
-          label: "Ayuda",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-    markdoc(),
-  ],
+      {
+        label: "Servicios REST",
+        items: [
+          // Each item here is one entry in the navigation menu
+          { label: "Verificar Correo Trabajador", slug: "api/verificar-correo" },
+        ],
+      },
+      {
+        label: "Referencias",
+        autogenerate: { directory: "reference" },
+      },
+      {
+        label: "Ayuda",
+        autogenerate: { directory: "reference" },
+      },
+    ],
+  }),markdoc()],
   devToolbar: {
     enabled: false,
   },
